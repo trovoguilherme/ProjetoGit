f7d8fde02341b3a0e6aa0ee28bb9b17a3519ba74
Guilherme Trovo Feliciano <guinavy2011@live.com>
2021-10-14 23:39:58 -0300

    Segundo commit

--- src/br/com/guilherme/Main.java
+++ src/br/com/guilherme/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    Pessoa pessoa = new Pessoa("12345678990", "Guilherme", 21);
+        Pessoa pessoa2 = new Pessoa("09876543221", "Bianca", 19);
+
 
         System.out.println(pessoa);
+
+        System.out.println(pessoa2);
+
+
     }
 }
