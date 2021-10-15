5c84df5e61aba76f4c9cc651bdfaff40c14cc36a
Guilherme Trovo Feliciano <guinavy2011@live.com>
2021-10-14 23:23:54 -0300

    Primeiro commit

---
+++ src/br/com/guilherme/Main.java
@@ -0,0 +1,10 @@
+package br.com.guilherme;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Pessoa pessoa = new Pessoa("12345678990", "Guilherme", 21);
+
+        System.out.println(pessoa);
+    }
+}
