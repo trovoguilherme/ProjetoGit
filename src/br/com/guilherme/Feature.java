73d4f5249e5f6bed13bde228595a891c847fcb03
Guilherme Trovo Feliciano <guinavy2011@live.com>
2021-10-14 23:51:28 -0300

    Nova feature - Branch

---
+++ src/br/com/guilherme/Feature.java
@@ -0,0 +1,30 @@
+package br.com.guilherme;
+
+public class Feature {
+    private long id;
+    private  String nome;
+
+    public Feature() {
+    }
+
+    public Feature(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/br/com/guilherme/Main.java
+++ src/br/com/guilherme/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    Pessoa pessoa = new Pessoa("12345678990", "Guilherme", 21);
         Pessoa pessoa2 = new Pessoa("09876543221", "Bianca", 19);
-
+        Feature f1 = new Feature();
 
         System.out.println(pessoa);
 
